7d7399485b5ea3d8c48d708c4ee67c117e3374eb
Seungpyo Suh <me@sspzoa.io>
2025-02-24 23:57:47 +0900

    Update index.ts

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,3 @@
 // src/components/common/index.ts
 export { default as RootWrapper } from './RootWrapper';
 export { default as LoadingSkeleton } from './LoadingSkeleton';
-export { default as CopyButton } from './CopyButton';
